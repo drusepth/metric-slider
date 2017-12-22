8cc75ee61d83dff10523e12b6d0e2baa1c5151d1
Andrew Brown <drusepth@gmail.com>
2017-12-22 15:54:43 -0600

    wip before railsassets

--- app/assets/javascripts/components/Document.jsx
+++ app/assets/javascripts/components/Document.jsx
@@ -5,15 +5,23 @@ window.Document = class Header extends React.Component {
     this.documentText = 'It was a dark and stormy night...'
   }
 
+  documentUpdateCallback(event) {
+    var newDocumentText = event.target.innerText;
+
+    console.log(newDocumentText);
+  }
+
   render() {
     return <div className="documentContainer">
       <div
         className="document"
         contentEditable="true"
         suppressContentEditableWarning
+        onBlur={ this.documentUpdateCallback }
       >
         { this.documentText }
       </div>
+      <input type="button" value="Update" />
     </div>
   }
 }
